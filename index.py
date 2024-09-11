68ebb72088828f3d9aa8f62590e72e37794f7605
lariciamelo <lariciamelo@gmail.com>
2024-09-11 15:19:47 -0300

    Programa de divisão de contas no restaurante

---
+++ index.py
@@ -0,0 +1,7 @@
+valor= float(input('Valor da conta: '))
+divisao = valor/3
+cada = int(divisao)
+sobra = valor - 2 * cada
+print('Carlos irá pagar R$ %.2f ' %cada)
+print('Andrá irá pagar R$ %.2f' %cada)
+print('Felipe irá pagar R$ %.2f ' %sobra)
